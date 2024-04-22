6535f8dfdaeb0b4daeaacfca6900d845e3698865
fadhil <fadhilirsyad187@gmail.com>
2024-04-22 14:21:07 +0700

    ubah warna Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,12 @@
 "use client"
-
 import React from 'react'
-
-
 
 export const Hero = ({heading, message}) => {
   return (
     <div>
         <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
             {/* overlay */}
-            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'/>
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-slate-500/70 z-[2]'/>
             <div className='p-40 text-white z-[2] mt-[-0rem] text-center'>
                 <h2 className='text-5xl font-bold'>{heading}</h2>
                 <div className='py-6 text-xl'> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 "use client";
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className='sticky left-0 top-0 w-full z-10 easy-in duration-300  '>
-        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white '>
-            <Link href='/'>
-                <h1 className='font-bold text-4xl '>Devscale</h1>
-            </Link>
-            <ul className='hidden sm:flex'>
-                <li className='p-4'> Home </li>
-                <li className='p-4'> Events </li>
-                <li className='p-4'> Contact </li>
-            </ul>
-        </div>
+    <div className="sticky left-0 top-0 w-full z-10 easy-in duration-300  ">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-black ">
+        <Link href="/">
+          <h1 className="font-bold text-3xl ">pixelpioneers.</h1>
+        </Link>
+        <ul className="hidden sm:flex">
+          <Link href="/">
+            <li className="p-4">Home</li>
+          </Link>
+          <Link href="/">
+            <li className="p-4"> Events</li>
+          </Link>
+          <Link href="/">
+            <li className="p-4"> Contact </li>
+          </Link>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
